e2711fa5fb16af6070af82b22e9a9e976046f20e
jafarov01 <73715394+jafarov01@users.noreply.github.com>
2021-01-19 00:29:44 +0100

    Update BattleShip.cpp

--- BattleShip.cpp
+++ BattleShip.cpp
@@ -1,3 +1,12 @@
+/* 
+*   Copyright (C) 2017-12?? Makhlug Jafarov
+*
+*   This code is made by Me, just as simple as shown below. You can freely take this code and modify if necessary. Glad this code will help you. Regards!
+    
+    //github.com/jafarov01
+    //https://www.linkedin.com/in/jafarov01/
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <windows.h>
